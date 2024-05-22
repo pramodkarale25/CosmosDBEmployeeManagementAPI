2e109ca75f237ca05302ecb97513f1a229c036b8
pramodkarale25 <pramodkarale.karale21@gmail.com>
2024-05-22 22:49:33 +0530

    Added TransactionBatch and Concurrency control .

---
+++ CosmosDBEmployeeManagementAPI/Controllers/ConcurrencyController.cs
@@ -0,0 +1,33 @@
+﻿using CosmosDBEmployeeManagementAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using System.Reflection.Metadata;
+
+namespace CosmosDBEmployeeManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConcurrencyController : ControllerBase
+    {
+        private record Product(string id, string name, string categoryId);
+        Product saddle = new Product("0120", "Worn Saddle", "accessories-used");
+        PartitionKey partitionKey = new PartitionKey("accessories-used");
+
+        private Container GetContainer()
+        {
+            return CosmosHelper.CreateDBAndContainer("ProductDB", "Product", "categoryId").Result;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpsertMultipleItem()
+        {
+            ItemResponse<Product> itemResponse = await GetContainer().ReadItemAsync<Product>(saddle.id, partitionKey);
+            saddle = new Product("0120", "Worn Saddle 55", "accessories-used");
+
+            ItemRequestOptions options = new ItemRequestOptions() { IfMatchEtag = itemResponse.ETag };
+            Product res = await GetContainer().UpsertItemAsync<Product>(saddle, partitionKey, options);
+            return Ok(res);
+        }
+    }
+}
---
+++ CosmosDBEmployeeManagementAPI/Controllers/TransactionBatchOperationController.cs
@@ -0,0 +1,98 @@
+﻿using CosmosDBEmployeeManagementAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Container = Microsoft.Azure.Cosmos.Container;
+
+namespace CosmosDBEmployeeManagementAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TransactionBatchOperationController : ControllerBase
+    {
+        //Batch operations only works with same partition key
+        private record Product(string id, string name, string categoryId);
+        Product saddle = new Product("0120", "Worn Saddle", "accessories-used");
+        Product handlebar = new Product("012A", "Rusty Handlebar", "accessories-used");
+        PartitionKey partitionKey = new PartitionKey("accessories-used");
+
+        private Container GetContainer()
+        {
+            return CosmosHelper.CreateDBAndContainer("ProductDB", "Product", "categoryId").Result;
+        }
+
+        private List<Product> GetItemCollection(TransactionalBatchResponse batchRes)
+        {
+            List<Product> lstProduct = new List<Product>();
+
+            if (batchRes.IsSuccessStatusCode)
+            {
+                TransactionalBatchOperationResult<Product> result;
+
+                for (int i = 0; i < batchRes.Count; i++)
+                {
+                    result = batchRes.GetOperationResultAtIndex<Product>(i);
+                    lstProduct.Add(result.Resource);
+                }
+            }
+
+            return lstProduct;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMultipleItem()
+        {
+            TransactionalBatch batch = GetContainer().CreateTransactionalBatch(partitionKey)
+                .CreateItem<Product>(saddle)
+                .CreateItem<Product>(handlebar);
+            using TransactionalBatchResponse batchRes = await batch.ExecuteAsync();
+            return Ok(GetItemCollection(batchRes));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ReadMultipleItem()
+        {
+            TransactionalBatch batch = GetContainer().CreateTransactionalBatch(partitionKey)
+                .ReadItem(saddle.id)
+                .ReadItem(handlebar.id);
+            using TransactionalBatchResponse batchRes = await batch.ExecuteAsync();
+            return Ok(GetItemCollection(batchRes));
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> ReplaceMultipleItem()
+        {
+            saddle = new Product("0120", "Worn Saddle 1", "accessories-used");
+            handlebar = new Product("012A", "Rusty Handlebar 1", "accessories-used");
+
+            TransactionalBatch batch = GetContainer().CreateTransactionalBatch(partitionKey)
+                .ReplaceItem<Product>(saddle.id, saddle)
+                .ReplaceItem<Product>(handlebar.id, handlebar);
+            using TransactionalBatchResponse batchRes = await batch.ExecuteAsync();
+            return Ok(GetItemCollection(batchRes));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpsertMultipleItem()
+        {
+            saddle = new Product("0120", "Worn Saddle 2", "accessories-used");
+            handlebar = new Product("012A", "Rusty Handlebar 2", "accessories-used");
+
+            TransactionalBatch batch = GetContainer().CreateTransactionalBatch(partitionKey)
+                .UpsertItem<Product>(saddle)
+                .UpsertItem<Product>(handlebar);
+            using TransactionalBatchResponse batchRes = await batch.ExecuteAsync();
+            return Ok(GetItemCollection(batchRes));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteMultipleItem()
+        {
+            TransactionalBatch batch = GetContainer().CreateTransactionalBatch(partitionKey)
+                .DeleteItem(saddle.id)
+                .DeleteItem(handlebar.id);
+            using TransactionalBatchResponse batchRes = await batch.ExecuteAsync();
+            return Ok(GetItemCollection(batchRes));
+        }
+    }
+}
